0bbe63f179fc78ebe932675becca94f3a9735748
yangfan2112 <145387043+yangfan2112@users.noreply.github.com>
2023-10-31 19:50:50 +0100

    Creando proyecto

---
+++ src/main/java/edu/cesur/fullstack/PracticaTipoExamenLibreriaApplication.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaTipoExamenLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaTipoExamenLibreriaApplication.class, args);
+	}
+
+}
